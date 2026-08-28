37bf5b7c3b22c063af458e2bd1ba8dbfd9a4a8b2
DaviDev <jdavidjimenezq@gmail.com>
2026-08-27 21:07:01 -0700

    Rename variable lista to arr in bubble sort exercises for English consistency

--- DUAD/Python Intermedio/EOOP/bubble_sort.py
+++ DUAD/Python Intermedio/EOOP/bubble_sort.py
@@ -1,10 +1,10 @@
-def bubble_sort(lista):
-    n = len(lista)
+def bubble_sort(arr):
+    n = len(arr)
     for i in range(n):
         for j in range(n - 1):
-            if lista[j] > lista[j + 1]:
-                lista[j], lista[j + 1] = lista[j + 1], lista[j]
-    return lista
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
 
 
 numbers = [5, 2, 8, 1, 9, 3]
